db6eb23b795e9de75cf6111ca38f4b509a33ad80
manasvi22sharma <sharmamanasvi2208@gmail.com>
2023-05-07 00:26:49 +0530

    update and delete topics

--- src/main/java/com/example/courseapp/topic/TopicController.java
+++ src/main/java/com/example/courseapp/topic/TopicController.java
@@ -3,7 +3,9 @@ import java.util.*;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 // spring is the one initalzing and creating object for controller class.
@@ -19,11 +21,27 @@ public class TopicController {
     }
 
     //{} mark the path as a parameter
-    @RequestMapping("/topics/{id}")
     //@pathVariable("id") to map the uri to input
     //@pathvaribale tells spring the id is a path variable and its valus will be picked form path
+    @RequestMapping("/topics/{id}")
     public Topic getTopic(@PathVariable String id){
         return topicservice.getTopic(id);
     }
+
     
+    @RequestMapping(method = RequestMethod.POST,value = "/topics")
+    public void addTopic ( @RequestBody Topic topic){
+        topicservice.addTopic(topic);
+    }
+
+    @RequestMapping(method = RequestMethod.DELETE,value = "/topics/{id}")
+    public void deleteTopic( @PathVariable String id){
+        topicservice.deleteTopic(id);
+    }
+
+    @RequestMapping(method = RequestMethod.PUT, value = "topics/{id}")
+    public void updateTopic(@RequestBody Topic topic, @PathVariable String id){
+        topicservice.updateTopic(id, topic);
+    }
+
 }
--- src/main/java/com/example/courseapp/topic/TopicService.java
+++ src/main/java/com/example/courseapp/topic/TopicService.java
@@ -1,5 +1,6 @@
 package com.example.courseapp.topic;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -8,15 +9,32 @@ import org.springframework.stereotype.Service;
 //mark this as business service
 @Service
 public class TopicService {
-    private List<Topic> topics= Arrays.asList(
+    //Arrays.asList makes an immutable list we cant add to it , hehce passed to a arraylist constructor.
+    private List<Topic> topics= new ArrayList<>(Arrays.asList(
         new Topic("spring","Spring","Spring framework course"),
         new Topic("java","JAVA Core","JAVA course"),
         new Topic("javascript","JavaScript","JavaScript course")
-    );
+    ));
     public List<Topic> getAllTopics(){
         return this.topics;
     }
     public Topic getTopic(String id){
+        //steam? java 8
         return topics.stream().filter(t -> t.getId().equals(id)).findFirst().get();
+    }
+    public void addTopic(Topic topic){
+        topics.add(topic);
+    }
+    public void deleteTopic(String id){
+        topics.removeIf(t-> t.getId().equals(id));
+    }
+    public void updateTopic(String id,Topic topic){
+       for(int i=0;i<topics.size();i++){
+        Topic t= topics.get(i);
+        if(t.getId().equals(id)){
+            topics.set(i,topic);
+            return;
+        }
+       }
     }
 }
