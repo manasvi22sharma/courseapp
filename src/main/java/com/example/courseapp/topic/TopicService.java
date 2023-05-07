1a9f64bc37f6a769bb6f8b23fc1e839b2961c7d8
manasvi22sharma <sharmamanasvi2208@gmail.com>
2023-05-07 20:22:13 +0530

    add spring data jpa for topics

--- src/main/java/com/example/courseapp/topic/TopicService.java
+++ src/main/java/com/example/courseapp/topic/TopicService.java
@@ -1,40 +1,53 @@
 package com.example.courseapp.topic;
 
 import java.util.ArrayList;
-import java.util.Arrays;
+//import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 //A service layer is a layer in an application that facilitates communication between the controller and the persistence layer.
 //mark this as business service
 @Service
 public class TopicService {
+
+    // adding repository
+    @Autowired
+    private TopicRepository topicRepo;
     //Arrays.asList makes an immutable list we cant add to it , hehce passed to a arraylist constructor.
-    private List<Topic> topics= new ArrayList<>(Arrays.asList(
-        new Topic("spring","Spring","Spring framework course"),
-        new Topic("java","JAVA Core","JAVA course"),
-        new Topic("javascript","JavaScript","JavaScript course")
-    ));
+    // private List<Topic> topics= new ArrayList<>(Arrays.asList(
+    //     new Topic("spring","Spring","Spring framework course"),
+    //     new Topic("java","JAVA Core","JAVA course"),
+    //     new Topic("javascript","JavaScript","JavaScript course")
+    // ));
     public List<Topic> getAllTopics(){
-        return this.topics;
+        List<Topic> topics =new ArrayList<>();
+        topicRepo.findAll().forEach(topics:: add);
+        return topics;
+        //return this.topics;
     }
     public Topic getTopic(String id){
         //steam? java 8
-        return topics.stream().filter(t -> t.getId().equals(id)).findFirst().get();
+        //return topics.stream().filter(t -> t.getId().equals(id)).findFirst().get();
+        return topicRepo.findById(id).orElse(null);
     }
     public void addTopic(Topic topic){
-        topics.add(topic);
+        topicRepo.save(topic);
+        //topics.add(topic);
     }
     public void deleteTopic(String id){
-        topics.removeIf(t-> t.getId().equals(id));
+        topicRepo.deleteById(id);
+        //topics.removeIf(t-> t.getId().equals(id));
     }
     public void updateTopic(String id,Topic topic){
-       for(int i=0;i<topics.size();i++){
-        Topic t= topics.get(i);
-        if(t.getId().equals(id)){
-            topics.set(i,topic);
-            return;
-        }
-       }
+
+        topicRepo.save(topic);
+    //    for(int i=0;i<topics.size();i++){
+    //     Topic t= topics.get(i);
+    //     if(t.getId().equals(id)){
+    //         topics.set(i,topic);
+    //         return;
+    //     }
+    //    }
     }
 }
